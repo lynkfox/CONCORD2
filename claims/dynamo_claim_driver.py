55e2b8184e6f366d133705517c14cf2d2685bec5
Lynkfox <lynkfox@gmail.com>
2021-01-08 14:48:49 -0500

    refactor for Timers to be pulled out into sub function

--- claims/dynamo_claim_driver.py
+++ claims/dynamo_claim_driver.py
@@ -5,15 +5,21 @@ from datetime import datetime, timedelta
 RECLAIMS = 1
 CLAIM_HOURS = 2
 
-def add_Claimant(member, system, base, table, *, deputy=None):
+def create_timers():
+    timer_tuple = collections.namedtuple("Timers", "now expires nowISO expiresISO")
+    timer_tuple.now =datetime.utcnow().replace(microsecond=0)
+    timer_tuple.expires = timer_tuple.now + timedelta(hours=CLAIM_HOURS)
+    timer_tuple.expiresISO = timer_tuple.expires.isoformat()
+    timer_tuple.nowISO = timer_tuple.now.isoformat()
     
-    now =datetime.utcnow().replace(microsecond=0)
-    expires = now + timedelta(hours=CLAIM_HOURS)
-    expiresISO = expires.isoformat()
-    nowISO = now.isoformat()
+    return timer_tuple
+
+def add_Claimant(member, system, base, table, *, deputy=None, loot="None Selected"):
+    
+    timers = create_timers()
     
     PK = "SYSTEM#"+system
-    SK = "CLAIMENDS#"+expiresISO
+    SK = "CLAIMENDS#"+timers.expiresISO
     deputy_store = collections.namedtuple('Member', 'display_name id')
     
     if deputy is None:
@@ -32,17 +38,22 @@ def add_Claimant(member, system, base, table, *, deputy=None):
             'System_Name': system,
             'Base_Level': str(base),
             'Reclaim_Number':str(0),
-            'Created_At': nowISO,
-            'Updated_at': nowISO,
-            'Expires_at': expiresISO,
+            'Created_At': timers.nowISO,
+            'Updated_at': timers.nowISO,
+            'Expires_at': timers.expiresISO,
             'Deputy_ID': deputy_store.id,
-            'Deputy_Tag':deputy_store.display_name
+            'Deputy_Tag':deputy_store.display_name,
+            'Loot_Distribution':loot
         }
     )
     
     return response
 
-# TODO: Add claimaint to the DB when they !claim [system]
+
+def check_Active_Claims(system, table):
+    timers = create_timers()
+    
+
 # TODO: Check for current claims active on a system
 # TODO: Update current claim to Release it or Reclaim it
 # TODO: End Claims that have expired
